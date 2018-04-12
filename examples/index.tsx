9aba7ffea61868da31a80c5134ba457ee02c9b1b
李梦龙 <649076408@qq.com>
2018-04-12 11:34:19 +0800

    feat: create

---
+++ examples/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+// 引入组件
+import { Icon } from '../src/component/index';
+// 引入样式
+import '../src/styles/index.scss';
+
+ReactDOM.render(<Icon />, document.getElementById('mountNode'));
