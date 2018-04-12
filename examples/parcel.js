2f64220686f49645d1fee64c611a13dda9470ac0
李梦龙 <649076408@qq.com>
2018-04-12 11:34:19 +0800

    feat: create

---
+++ examples/parcel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Icon } from '../dist/rcmd-test';
+
+ReactDOM.render(
+  <Icon type="primary">Icon</Icon>
+, document.getElementById('mountNode'));
---
+++ src/component/Icon.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class Icon extends React.Component {
+  static __ANT_BUTTON = true
+  render() {
+    return (
+      <div className="aaa">一个组件</div>
+    );
+  }
+}
+
+export default Icon;
